69e7309ceed4c9d2968768a4cad4d47c597d2738
Driptanil Datta <53353462+driptanil@users.noreply.github.com>
2024-10-11 00:09:11 +0530

    add: log config

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -8,8 +8,11 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[tracing::instrument]
 #[get("/version")]
 async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
+    tracing::info!("Getting version");
+
     let result: Result<String, sqlx::Error> = sqlx
         ::query_scalar("SELECT version()")
         .fetch_one(db.get_ref()).await;
