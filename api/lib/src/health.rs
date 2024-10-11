0e1ac5cb0c32ddb11c3f9d8decd3be987529e951
Driptanil Datta <53353462+driptanil@users.noreply.github.com>
2024-10-12 02:02:31 +0530

    add: health unit test

--- api/lib/src/health.rs
+++ api/lib/src/health.rs
@@ -27,3 +27,25 @@ async fn version(db: web::Data<PgPool>) -> String {
 async fn health() -> HttpResponse {
     HttpResponse::Ok().append_header(("version", "0.0.1")).finish()
 }
+
+#[cfg(test)]
+mod test {
+    use actix_web::http::StatusCode;
+
+    use super::*;
+
+    #[actix_rt::test]
+    async fn health_check_works() {
+        let response = health().await;
+
+        assert!(response.status().is_success());
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let data = response
+            .headers()
+            .get("version")
+            .and_then(|v| v.to_str().ok());
+
+        assert_eq!(data, Some("0.0.1"));
+    }
+}
