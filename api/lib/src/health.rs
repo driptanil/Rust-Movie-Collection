2923eb169135c6601c45cf8bcfe03b07bff81791
Driptanil Datta <53353462+driptanil@users.noreply.github.com>
2024-10-12 12:26:59 +0530

    add: health integration test

--- api/lib/src/health.rs
+++ api/lib/src/health.rs
@@ -23,14 +23,16 @@ async fn version(db: web::Data<PgPool>) -> String {
     }
 }
 
+const API_VERSION: &str = "0.0.1";
+
 #[tracing::instrument]
 async fn health() -> HttpResponse {
-    HttpResponse::Ok().append_header(("version", "0.0.1")).finish()
+    HttpResponse::Ok().append_header(("version", API_VERSION)).finish()
 }
 
 #[cfg(test)]
 mod test {
-    use actix_web::http::StatusCode;
+    use actix_web::{ http::StatusCode, App };
 
     use super::*;
 
@@ -46,6 +48,27 @@ mod test {
             .get("version")
             .and_then(|v| v.to_str().ok());
 
-        assert_eq!(data, Some("0.0.1"));
+        assert_eq!(data, Some(API_VERSION));
+    }
+
+    #[actix_rt::test]
+    async fn health_check_works_endpoint() {
+        let app = App::new().configure(service);
+
+        let mut app = actix_web::test::init_service(app).await;
+
+        let request = actix_web::test::TestRequest::get().uri("/health").to_request();
+
+        let response = actix_web::test::call_service(&mut app, request).await;
+
+        assert!(response.status().is_success());
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let data = response
+            .headers()
+            .get("version")
+            .and_then(|v| v.to_str().ok());
+
+        assert_eq!(data, Some(API_VERSION))
     }
 }
