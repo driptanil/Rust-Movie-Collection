972dad5c0a1454daa3c490c6d1db08550c99503b
Driptanil Datta <53353462+driptanil@users.noreply.github.com>
2024-10-12 15:20:54 +0530

    add: movies model traits

--- shared/src/models/movie.rs
+++ shared/src/models/movie.rs
@@ -1,5 +1,7 @@
+use serde::{ Deserialize, Serialize };
 use sqlx::types::{ Uuid, chrono::{ DateTime, Utc } };
 
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct Movie {
     pub id: Uuid,
     pub title: String,
@@ -9,7 +11,7 @@ pub struct Movie {
     pub created_at: Option<DateTime<Utc>>,
     pub updated_at: Option<DateTime<Utc>>,
 }
-
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct CreateMovieRequest {
     pub title: String,
     pub director: String,
