5336341e126b1862ca770e23e11d16dcedb9181c
Driptanil Datta <53353462+driptanil@users.noreply.github.com>
2025-01-16 23:22:50 +0530

    movie analysis notebook

---
+++ notebooks/src/main.rs
@@ -0,0 +1,25 @@
+use polars::prelude::*;
+
+fn main() {
+    let q = LazyCsvReader::new("./data/amazon_prime_titles.csv")
+        .with_has_header(true)
+        .finish()
+        .unwrap();
+
+    let df = q.collect().unwrap();
+
+    let df_listed_in = df
+        .clone()
+        .lazy()
+        .select([
+            col("listed_in"),
+            // col("listed_in")
+            //     .apply(|s| { Ok(s.str()?.str_len_chars()) }, GetOutput::from_type(DataType::UInt32))
+            //     .alias("listed_in_exploded"),
+            col("listed_in").cast(DataType::).alias("listed_in_exploded"),
+        ])
+        .collect()
+        .unwrap();
+
+    println!("{:?}", df_listed_in);
+}
